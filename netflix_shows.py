e248b50f329550230f9377019b71ec922ebbc345
Michael Scofield <oscarliu97@outlook.com>
2021-02-08 22:17:32 +0800

    1.3

--- netflix_shows.py
+++ netflix_shows.py
@@ -11,7 +11,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("netflix_titles.csv")
+netflix_shows = pd.read_csv("netflix_titles.csv")
 
-order=df['rating'].value_counts().index[0:14]
+netflix_date = netflix_shows[['date_added']].dropna()
+netflix_date['year'] = netflix_date['date_added'].apply(lambda x : x.split(', ')[-1])
+netflix_date['month'] = netflix_date['date_added'].apply(lambda x : x.lstrip().split(' ')[0])
 
+month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][::-1]
+df = netflix_date.groupby('year')['month'].value_counts().unstack().fillna(0)[month_order].T
+plt.figure(figsize=(10, 7), dpi=200)
+plt.pcolor(df, cmap='afmhot_r', edgecolors='white', linewidths=2) # heatmap
+plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns, fontsize=7, fontfamily='serif')
+plt.yticks(np.arange(0.5, len(df.index), 1), df.index, fontsize=7, fontfamily='serif')
+
+plt.title('Netflix Contents Update', fontsize=12, fontfamily='calibri', fontweight='bold', position=(0.20, 1.0+0.02))
+cbar = plt.colorbar()
+
+cbar.ax.tick_params(labelsize=8) 
+cbar.ax.minorticks_on()
+plt.show()
