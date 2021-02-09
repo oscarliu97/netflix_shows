54561ba6ded4b00b7b893e0013234ba54722aa77
Michael Scofield <oscarliu97@outlook.com>
2021-02-09 15:21:04 +0800

    1.5

--- netflix_shows.py
+++ netflix_shows.py
@@ -10,49 +10,24 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
+from collections import Counter
 
 netflix_shows = pd.read_csv("netflix_titles.csv")
 
-netflix_date = netflix_shows[['date_added']].dropna()
-netflix_date['year'] = netflix_date['date_added'].apply(lambda x : x.split(', ')[-1])
-netflix_date['month'] = netflix_date['date_added'].apply(lambda x : x.lstrip().split(' ')[0])
-netflix_shows['principal_country'] = netflix_shows['country'].apply(lambda x: x.split(",")[0])
-month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][::-1]
-df = netflix_date.groupby('year')['month'].value_counts().unstack().fillna(0)[month_order].T
-plt.figure(figsize=(10, 7), dpi=200)
-plt.pcolor(df, cmap='afmhot_r', edgecolors='white', linewidths=2) # heatmap
-plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns, fontsize=7, fontfamily='serif')
-plt.yticks(np.arange(0.5, len(df.index), 1), df.index, fontsize=7, fontfamily='serif')
-
-plt.title('Netflix Contents Update', fontsize=12, fontfamily='calibri', fontweight='bold', position=(0.20, 1.0+0.02))
-cbar = plt.colorbar()
-
-cbar.ax.tick_params(labelsize=8) 
-cbar.ax.minorticks_on()
-plt.show()
-a = np.arange(0.5, len(df.columns), 1)
-ratings_ages = {
-    'TV-PG': 'Older Kids',
-    'TV-MA': 'Adults',
-    'TV-Y7-FV': 'Older Kids',
-    'TV-Y7': 'Older Kids',
-    'TV-14': 'Teens',
-    'R': 'Adults',
-    'TV-Y': 'Kids',
-    'NR': 'Adults',
-    'PG-13': 'Teens',
-    'TV-G': 'Kids',
-    'PG': 'Older Kids',
-    'G': 'Kids',
-    'UR': 'Adults',
-    'NC-17': 'Adults'
-}
-netflix_shows['target_ages'] = netflix_shows['rating'].replace(ratings_ages)
-rating_df = netflix_shows.groupby(['rating', 'target_ages']).agg({'show_id': 'count'}).reset_index()
-rating_df.columns = ['rating','target_ages','count']
-rating_df = rating_df.sort_values('target_ages')
-
-country_df = df['principal_country'].value_counts().reset_index()
+netflix_shows['genre'] = netflix_shows['listed_in'].apply(lambda x :  x.replace(' ,',',').replace(', ',',').split(',')) 
 # country_df = country_df[country_df['principal_country'] /  country_df['principal_country'].sum() > 0.01]
+d2 = netflix_shows[netflix_shows['type'] == 'Movie']
+col = "listed_in"
+categories = ", ".join(d2['listed_in']).split(", ")
+counter_list = Counter(categories).most_common(50)
+labels = [_[0] for _ in counter_list][::-1]
+values = [_[1] for _ in counter_list][::-1]
+trace1 = go.Bar(y=labels, x=values, orientation="h", name="TV Shows", marker=dict(color="#a678de"))
 
-netflix_shows
+data = [trace1]
+layout = go.Layout(title="Content added over the years", legend=dict(x=0.1, y=1.1, orientation="h"))
+fig = go.Figure(data, layout=layout)
+fig.show()
+
+text = str(list(d2['genre'])).replace(',', '').replace('[', '').replace("'", '').replace(']', '')
