86a46c9b712955ad967e34401931884f9347118a
Michael Scofield <oscarliu97@outlook.com>
2021-02-09 12:11:34 +0800

    1.4

--- netflix_shows.py
+++ netflix_shows.py
@@ -16,7 +16,7 @@ netflix_shows = pd.read_csv("netflix_titles.csv")
 netflix_date = netflix_shows[['date_added']].dropna()
 netflix_date['year'] = netflix_date['date_added'].apply(lambda x : x.split(', ')[-1])
 netflix_date['month'] = netflix_date['date_added'].apply(lambda x : x.lstrip().split(' ')[0])
-
+netflix_shows['principal_country'] = netflix_shows['country'].apply(lambda x: x.split(",")[0])
 month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][::-1]
 df = netflix_date.groupby('year')['month'].value_counts().unstack().fillna(0)[month_order].T
 plt.figure(figsize=(10, 7), dpi=200)
@@ -30,3 +30,29 @@ cbar = plt.colorbar()
 cbar.ax.tick_params(labelsize=8) 
 cbar.ax.minorticks_on()
 plt.show()
+a = np.arange(0.5, len(df.columns), 1)
+ratings_ages = {
+    'TV-PG': 'Older Kids',
+    'TV-MA': 'Adults',
+    'TV-Y7-FV': 'Older Kids',
+    'TV-Y7': 'Older Kids',
+    'TV-14': 'Teens',
+    'R': 'Adults',
+    'TV-Y': 'Kids',
+    'NR': 'Adults',
+    'PG-13': 'Teens',
+    'TV-G': 'Kids',
+    'PG': 'Older Kids',
+    'G': 'Kids',
+    'UR': 'Adults',
+    'NC-17': 'Adults'
+}
+netflix_shows['target_ages'] = netflix_shows['rating'].replace(ratings_ages)
+rating_df = netflix_shows.groupby(['rating', 'target_ages']).agg({'show_id': 'count'}).reset_index()
+rating_df.columns = ['rating','target_ages','count']
+rating_df = rating_df.sort_values('target_ages')
+
+country_df = df['principal_country'].value_counts().reset_index()
+# country_df = country_df[country_df['principal_country'] /  country_df['principal_country'].sum() > 0.01]
+
+netflix_shows
